e9ed376f21c340aacbc48e3794ed7ad5b5e32b81
Freyzzer <freyzzergvm12@gmail.com>
2025-01-22 20:38:38 -0500

    testing para el formulario

--- src/app/components/form-ingreso/form-ingreso.component.spec.ts
+++ src/app/components/form-ingreso/form-ingreso.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FormIngresoComponent } from './form-ingreso.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('FormIngresoComponent', () => {
   let component: FormIngresoComponent;
@@ -8,7 +8,7 @@ describe('FormIngresoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FormIngresoComponent]
+      imports: [FormIngresoComponent, ReactiveFormsModule]
     })
     .compileComponents();
 
@@ -20,4 +20,40 @@ describe('FormIngresoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('debería inicializar el formulario con el campo "partida" vacío', () => {
+    const form = component.formIngreso;
+    expect(form.get('partida')?.value).toBe('');
+  });
+
+  it('debería marcar el formulario inválido si el campo "partida" está vacío', () => {
+    const form = component.formIngreso;
+    form.get('partida')?.setValue('');
+    expect(form.valid).toBeFalse();
+  });
+
+  it('debería ser inválido si el nombre contiene caracteres especiales', () => {
+    const form = component.formIngreso;
+    form.get('partida')?.setValue('Partida@#');
+    expect(form.valid).toBeFalse();
+  });
+
+  it('debería ser inválido si el nombre contiene más de 3 números', () => {
+    const form = component.formIngreso;
+    form.get('partida')?.setValue('Partida1234');
+    expect(form.valid).toBeFalse();
+  });
+
+  // it('debería ser inválido si el nombre tiene menos de 5 caracteres', () => {
+  //   const form = component.formIngreso;
+  //   form.get('partida')?.setValue('1234');
+  //   expect(form.valid).toBeFalse();
+  // });
+
+  it('debería ser inválido si el nombre contiene solo números', () => {
+    const form = component.formIngreso;
+    form.get('partida')?.setValue('12345');
+    expect(form.valid).toBeFalse();
+  });
+
 });
